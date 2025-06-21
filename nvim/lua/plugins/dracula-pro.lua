bdb8fbf338b7c22185125613cd9b42bcc517d9ae
Nathan Whyte <nathanwhyte35@gmail.com>
2025-06-21 18:45:40 -0400

    Fix Lazy import issue

--- nvim/lua/plugins/dracula-pro.lua
+++ nvim/lua/plugins/dracula-pro.lua
@@ -2,8 +2,7 @@ return {
   dir = '/Users/natew/.config/nvim/themes/dracula_pro/',
   lazy = false,
   priority = 1000, -- Make sure to load this before all the other start plugins.
-  config = function()
-    require 'dracula_pro'
+  init = function()
     vim.cmd.colorscheme 'dracula_pro_van_helsing'
   end,
 }
--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -2,14 +2,16 @@
 -- https://github.com/folke/tokyonight.nvim
 
 return {
-  'folke/tokyonight.nvim',
-  priority = 1000, -- load this before all the other start plugins
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require('tokyonight').setup {
-      styles = {
-        comments = { italic = false }, -- Disable italics in comments
-      },
-    }
-  end,
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000, -- load this before all the other start plugins
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('tokyonight').setup {
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      }
+    end,
+  },
 }
