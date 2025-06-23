9bbb533bea5def28afaa2be3542a7577fcd4e685
Nathan Whyte <nathanwhyte35@gmail.com>
2025-06-23 07:44:57 -0500

    Add a simple bufferline via bufferline.nvim

---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,21 @@
+-- adds a vscode-like bufferline
+-- https://github.com/akinsho/bufferline.nvim
+
+return {
+  'akinsho/bufferline.nvim',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    require('bufferline').setup {
+      options = {
+        diagnostics = 'nvim_lsp',
+        diagnostics_indicator = function(count, level)
+          local icon = level:match 'error' and ' ' or ' '
+          return ' ' .. icon .. count
+        end,
+      },
+    }
+
+    vim.keymap.set('n', '<leader>bp', '<cmd>BufferLinePick<CR>', { desc = 'Pick an active buffer' })
+    vim.keymap.set('n', 'Bp', '<cmd>BufferLinePick<CR>', { desc = 'Pick an active buffer' })
+  end,
+}
